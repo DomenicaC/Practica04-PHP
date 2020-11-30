5dbcc2b52e57f506961a9d7d5f459b6a0571fe27
Domenica Vintimilla <49033368+DomenicaC@users.noreply.github.com>
2020-11-29 23:05:41 -0500

    Creacion de carpetas
    Conexión a base de datos
    Crear usuarios

---
+++ SistemaDeGestion/public/controladores/login.php
@@ -0,0 +1,25 @@
+<?php
+echo "<p>Hola mundo !!!</p>";
+
+    session_start();
+    //CONEXION BASE DE DATOS    
+    include '../../config/conexionBD.php';
+
+    $usuario = isset($_POST["correo"]) ? trim($_POST["correo"]) : null;
+    $contrasena = isset($_POST["contrasena"]) ? trim($_POST["contrasena"]) : null;
+
+    $sql = "SELECT * FROM usuario WHERE usu_correo = '$usuario' and usu_password = MD5('$contrasena')";
+
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        $_SESSION['isLogged'] = TRUE;
+        header("Location: ../../admin/vista/usuario/index.php");
+        echo "<p>ingreso!! :)</p>";
+    } else {
+        header("Location: ../vista/login.html");
+        echo "<p>no se pudo ingresar!! :)</p>";
+
+    }
+
+    $conn->close();
+?>
